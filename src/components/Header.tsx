066a2c7f0d1960e10bba1d380a4d617a9fc41f22
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-05-13 17:34:41 -0600

    ✨ Header component with form

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,104 @@
-import { Link } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+import {
+  HeartIcon,
+  SparklesIcon,
+  MagnifyingGlassIcon,
+} from "@heroicons/react/24/outline";
+import { useMemo } from "react";
 
 export default function Header() {
-  return (
-    <header className="bg-zinc-900 text-white w-full shadow-sm">
-      <div className="container mx-auto md:max-w-2xl lg:max-w-5xl px-2 py-8">
-        <div className="flex flex-col md:flex-row  gap-10 md:gap-0 items-center justify-between">
-          <Link to="/" className="flex items-center gap-3">
-            <img src="/drinky.png" alt="Drinky Logo" className="h-35 w-auto" />
-          </Link>
+  const { pathname } = useLocation();
+  const isHome = useMemo(() => pathname === "/", [pathname]);
 
-          <nav className="flex space-x-8 text-lg uppercase font-bold">
-            <Link
+  return (
+    <header
+      className={`transition-opacity duration-1000 ease-out
+    ${
+      isHome
+        ? "relative bg-[url('/background.jpg')] bg-center bg-cover opacity-0 animate-fadeIn"
+        : "bg-zinc-900 text-white w-full shadow-sm"
+    }`}
+    >
+      {isHome && (
+        <>
+          <div className="absolute inset-0 bg-gradient-to-t from-black/80 via-fuchsia-950/80 to-black/60 z-0" />
+          <div className="absolute inset-0 bg-black/20 backdrop-blur-xs z-0" />
+        </>
+      )}
+
+      <div className="relative z-10 container mx-auto md:max-w-2xl lg:max-w-5xl px-2 py-8">
+        <div className="flex flex-col md:flex-row gap-10 md:gap-0 items-center justify-between">
+          <NavLink to="/" className="flex items-center gap-3">
+            <img src="/drinky.png" alt="Drinky Logo" className="h-35 w-auto" />
+          </NavLink>
+
+          <nav className="flex space-x-8 text-md uppercase">
+            <NavLink
               to="/favorites"
-              className="text-zinc-100 hover:text-fuchsia-400 transition-colors duration-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-fuchsia-400 rounded"
+              className={({ isActive }) =>
+                isActive
+                  ? "flex items-center gap-2 text-fuchsia-400 transition-colors duration-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-fuchsia-400 rounded"
+                  : "flex items-center gap-2 text-zinc-100 hover:text-fuchsia-400 transition-colors duration-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-fuchsia-400 rounded"
+              }
             >
+              <HeartIcon className="h-5 w-5" />
               Favoritos
-            </Link>
-            <a
-              href="#"
-              className="text-zinc-100 hover:text-fuchsia-400 transition-colors duration-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-fuchsia-400 rounded"
+            </NavLink>
+            <NavLink
+              to="/create-with-ai"
+              className={({ isActive }) =>
+                isActive
+                  ? "flex items-center gap-2 text-fuchsia-400 transition-colors duration-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-fuchsia-400 rounded"
+                  : "flex items-center gap-2 text-zinc-100 hover:text-fuchsia-400 transition-colors duration-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-fuchsia-400 rounded"
+              }
             >
-              Crea con ia
-            </a>
+              <SparklesIcon className="h-5 w-5" />
+              Crea con IA
+            </NavLink>
           </nav>
         </div>
+
+       
+        {isHome && (
+          <div className="flex flex-col items-center justify-center animate-fadeIn delay-300 py-15">
+            <h2 className="capitalize text-3xl text-center font-bold text-white mt-10 mb-15 opacity-0 animate-fadeInUp">
+              encuentra las mejores recetas de bebidas
+            </h2>
+            <form
+              onSubmit={(e) => e.preventDefault()}
+              className="w-full max-w-3xl bg-white/20 backdrop-blur-lg p-6 rounded-lg flex flex-col sm:flex-row sm:space-x-4 space-y-4 sm:space-y-0 opacity-0 animate-fadeInUp delay-500"
+            >
+              <input
+                type="text"
+                placeholder="Ingredientes"
+                className="flex-1 px-4 py-2 rounded-md bg-white/30 placeholder-white/70 text-white focus:outline-none focus:ring-2 focus:ring-fuchsia-400"
+              />
+
+              <select className="px-4 cursor-pointer py-2 rounded-md bg-white/30 text-white focus:outline-none focus:ring-2 focus:ring-fuchsia-400">
+                <option value="" disabled selected className="text-black/70">
+                  Categoría
+                </option>
+                <option value="cocteles" className="text-black">
+                  Cócteles
+                </option>
+                <option value="mocktails" className="text-black">
+                  Mocktails
+                </option>
+                <option value="shots" className="text-black">
+                  Shots
+                </option>
+              </select>
+
+              <button
+                type="submit"
+                className="px-6 py-2 bg-fuchsia-500 hover:bg-fuchsia-600 rounded-md text-white flex items-center justify-center focus:outline-none focus:ring-2 focus:ring-fuchsia-400 cursor-pointer"
+              >
+                <MagnifyingGlassIcon className="h-5 w-5" />
+                <span className="md:sr-only ml-2 md:ml-0">Buscar</span>
+              </button>
+            </form>
+          </div>
+        )}
       </div>
     </header>
   );
