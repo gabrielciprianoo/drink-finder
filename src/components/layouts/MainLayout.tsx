6d6b342ffa03ac8b8a30fd961a4954b026ff3de6
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-05-28 17:32:09 -0600

    ♻️ main layout modified to just show in others pages diferents to index

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -1,13 +1,21 @@
-import { Outlet } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import Header from "../Header";
+import { Outlet } from "react-router-dom";
 
 export default function MainLayout() {
+  const { pathname } = useLocation();
+  const showMainWrapper = pathname !== "/"; 
+
   return (
     <>
       <Header />
-      <main className="container mx-auto md:max-w-2xl lg:max-w-5xl py-16 px-2">
+      {showMainWrapper ? (
+        <main className="container mx-auto md:max-w-2xl lg:max-w-5xl px-2">
+          <Outlet />
+        </main>
+      ) : (
         <Outlet />
-      </main>
+      )}
     </>
   );
 }
