5c63190623c2090ea5e573f82dcef7c760734bc0
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-05-28 17:35:33 -0600

    ✨ logic to search with filters and loading added

--- src/stores/recipeSlice.ts
+++ src/stores/recipeSlice.ts
@@ -1,20 +1,54 @@
 import type { StateCreator } from "zustand";
-import { getCategories } from "../services/RecipeService";
-import type { Categories } from "../types";
+import { getCategories, getRecipes } from "../services/RecipeService";
+import type { Categories, Drinks, SearchFilterType } from "../types";
 
 export type RecipeSliceType = {
-    categories: Categories;
-    fetchCategories: () => Promise<void>;
-}
+  categories: Categories;
+  drinks: Drinks;
+  loading: boolean;
+  filters: SearchFilterType;
+  fetchCategories: () => Promise<void>;
+  searchRecipers: (searchFilters: SearchFilterType) => Promise<void>;
+};
 
-export const recipeSlice : StateCreator<RecipeSliceType> =  ( set ) => ({
-    categories: {
-        drinks: []
-    },
-    fetchCategories: async () =>{
-        const categories = await getCategories();
-        set({
-            categories
-        })
+export const recipeSlice: StateCreator<RecipeSliceType> = (set) => ({
+  categories: {
+    drinks: [],
+  },
+
+  drinks: {
+    drinks: [],
+  },
+
+  loading: false,
+
+  filters: {
+    ingredient: '',
+    category: ''
+  },
+
+  fetchCategories: async () => {
+    const categories = await getCategories();
+    set({
+      categories,
+    });
+  },
+
+  searchRecipers: async (filters) => {
+    try {
+      set({ loading: true });
+
+      const [drinks] = await Promise.all([
+        getRecipes(filters),
+        new Promise((r) => setTimeout(r, 2000)),
+      ]);
+      set({ filters })
+      set({ drinks });
+    } catch (error) {
+      console.error("Error al buscar recetas", error);
+      set({ drinks: { drinks: [] } });
+    } finally {
+      set({ loading: false });
     }
+  },
 });
