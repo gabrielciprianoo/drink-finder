b879babeeb47cbb8afc3567e3c6769f8b780e452
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-05-28 17:34:28 -0600

    ✨ get recipes added to get recipes data and validate with valibot

--- src/services/RecipeService.ts
+++ src/services/RecipeService.ts
@@ -1,14 +1,33 @@
 import axios from "axios";
-import { parse } from "valibot";
-import { CategoriesAPISchema } from "../schemas";
+import { parse, safeParse } from "valibot";
+import { CategoriesAPISchema, DrinksAPIResponseSchema } from "../schemas";
+import type { SearchFilterType } from "../types";
 
-export async function getCategories()  {
-    const url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
-    const { data }  =  await axios(url);
-    const response = parse(CategoriesAPISchema, data );
+export async function getCategories() {
+  const url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list";
+  const { data } = await axios(url);
+  const response = parse(CategoriesAPISchema, data);
 
-    if(response){
-        return response;
+  if (response) {
+    return response;
+  }
+}
+
+export async function getRecipes(filters: SearchFilterType) {
+  try {
+    const { category, ingredient } = filters;
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=${category}&i=${ingredient}`;
+
+    const { data } = await axios(url);
+    const result = safeParse(DrinksAPIResponseSchema, data);
+    if (!result.success) {
+      console.log("data is not definded correctly");
+      return;
     }
-    
+
+    const { output } = result;
+    return output;
+  } catch (e) {
+    console.error(e);
+  }
 }
