ccf96df1e36642b14914cb6be2c588723045bbe7
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-06-19 14:07:45 -0600

    💄 add animation hover in card

--- src/components/DrinkCard.tsx
+++ src/components/DrinkCard.tsx
@@ -2,22 +2,27 @@ import { useAppStore } from "../stores/useAppStore";
 import type { Drink } from "../types";
 
 type DrinkCardProps = {
-  drink: Drink
+  drink: Drink;
 };
 
 export default function DrinkCard({ drink }: DrinkCardProps) {
-  const selectRecipe = useAppStore( ( s ) =>  s.selectRecipe);
+  const selectRecipe = useAppStore((s) => s.selectRecipe);
   const { idDrink: id, strDrink: name, strDrinkThumb: image } = drink;
 
   return (
-    <div className="bg-zinc-900 rounded-2xl border border-zinc-800 shadow-[0_4px_20px_rgba(0,0,0,0.3)] hover:shadow-fuchsia-500/20 hover:border-fuchsia-500/60 transition-all duration-300 overflow-hidden cursor-pointer"
-      onClick={ () => { selectRecipe( id ) }}
+    <div
+      className="bg-zinc-900 rounded-2xl border border-zinc-800 shadow-[0_4px_20px_rgba(0,0,0,0.3)] hover:shadow-fuchsia-500/20 hover:border-fuchsia-500/60 transition-all duration-300 overflow-hidden cursor-pointer"
+      onClick={() => {
+        selectRecipe(id);
+      }}
     >
-      <img
-        src={image}
-        alt={name}
-        className="w-full aspect-[3/4] object-cover"
-      />
+      <div className="overflow-hidden aspect-[3/4]">
+        <img
+          src={image}
+          alt={name}
+          className="w-full h-full object-cover transition-transform duration-300 ease-in-out hover:scale-110 hover:rotate-2"
+        />
+      </div>
       <div className="p-4">
         <h3 className="text-base font-semibold text-white/90 leading-tight truncate">
           {name}
