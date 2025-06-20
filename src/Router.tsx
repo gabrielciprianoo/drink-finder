149270e16ad86d8d8addd5558c5a1ec010e1b3e6
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-06-20 14:57:17 -0600

    ⚡️ improve performace with suspense and lazy using react router dom

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,27 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { FavoritesPage, IndexPage } from "./pages";
+import { lazy, Suspense} from 'react'
 import MainLayout from "./components/layouts/MainLayout";
+
+
+const FavoritesPage = lazy( () => import('./pages/FavoritesPage'));
+
+const IndexPage = lazy(() => import('./pages/IndexPage'));
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<MainLayout />}>
-          <Route path="/" element={<IndexPage />} />
-          <Route path="/favorites" element={<FavoritesPage />} />
+          <Route path="/" element={
+            <Suspense fallback={'cargando...'}>
+              <IndexPage />
+            </Suspense>
+          } />
+          <Route path="/favorites" element={
+            <Suspense fallback={'cargando...'}>
+              <FavoritesPage />
+            </Suspense>
+          } />
         </Route>
       </Routes>
     </BrowserRouter>
