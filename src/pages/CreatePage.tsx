0d4edfeb8cf9d8a5d69e34f3b23766aee5717c41
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-06-21 16:54:16 -0600

    ✨ implement recipe generation display in CreatePage component

--- src/pages/CreatePage.tsx
+++ src/pages/CreatePage.tsx
@@ -1,6 +1,18 @@
+import { useMemo } from "react";
 import CreateRecipeForm from "../components/CreateRecipeForm";
+import { useAppStore } from "../stores/useAppStore";
+import DrinksList from "../components/DrinksList";
+import Modal from "../components/Modal";
 
 export default function CreatePage() {
+  const generatedRecipes = useAppStore((s) => s.generatedRecipes.drinks);
+
+  const hasRecipes = useMemo(
+    () => generatedRecipes.length > 0,
+    [generatedRecipes.length]
+  );
+
+  console.log(generatedRecipes);
 
   return (
     <section className="max-w-3xl mx-auto px-4 py-12 text-white">
@@ -14,6 +26,15 @@ export default function CreatePage() {
 
       <CreateRecipeForm />
 
+      {hasRecipes && (
+        <>
+          <h2 className="text-3xl md:text-5xl font-extrabold text-center text-fuchsia-400  my-20">
+            Ultimas Recetas Generadas
+          </h2>
+          <DrinksList drinks={generatedRecipes} />
+        </>
+      )}
+      <Modal/>
     </section>
   );
 }
