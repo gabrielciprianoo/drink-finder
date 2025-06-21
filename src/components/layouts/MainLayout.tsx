13e7b0918a8bdadfea28a4e7d548188ac69f9cc9
gabrielciprianoo <devgabrielcipriano@gmail.com>
2025-06-21 16:53:50 -0600

    ✨ load generated data from storage in MainLayout component

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -9,8 +9,10 @@ export default function MainLayout() {
   const { pathname } = useLocation();
   const showMainWrapper = pathname !== "/"; 
   const loadFromStorage = useAppStore( (s) => s.loadFromStorage);
+  const loadGeneratedFromStorage = useAppStore( (s) => s.loadGeneratedFromStorage);
   useEffect(()=>{
-    loadFromStorage()
+    loadFromStorage();
+    loadGeneratedFromStorage();
   },[])
   return (
     <>
